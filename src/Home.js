91d2be2b3ba7f11b32f96f35bd79c636e5839a16
chetanakashV <=>
2024-04-11 11:09:59 +0000

    second commit

--- src/Home.js
+++ src/Home.js
@@ -1,82 +1,74 @@
 import React, { useEffect, useState } from "react";
 import * as redis from 'redis'; 
+import Axios from 'axios'
+var subscriber, publisher;
 
- 
+// const url = 'nidhicache-0001-001.cngw3p.0001.use1.cache.amazonaws.com'; 
 
-var subscriber, publisher, client;; 
 
-const url = 'nidhicache-0001-001.cngw3p.0001.use1.cache.amazonaws.com'; 
+const client = new WebSocket('ws://localhost:8081');
+    client.onopen = () => {
+      console.log('ws opened on browser')
+     
+    }
 
+    client.onmessage = (message) =>{
+        console.log(message.data)
+    }
+
+    const publishMessage = (channel, message) => {
+        client.send(JSON.stringify({
+            type: "publish", 
+            channelName: channel, 
+            mess: message
+        }))
+    }
+
+    const subscribeChannel = (channel) =>{
+        client.send(JSON.stringify({
+            type: "subscribe", 
+            channelName: channel
+        }))
+    }
+
+    const sendMessage = () =>{
+        client.send(JSON.stringify({
+            type: "subscribe", 
+            channelName: "test444"
+        }))
+    }
+    
 
 const Home = () =>{
 
-    const [refresh, setRefresh] = useState(false)
+    const [channelName, setChannelName] = useState(""); 
+    const [message, setMessage] = useState(""); 
+    const [subChannelName, setSubChannelName] = useState("")
 
-    const createPublisher = () =>{
-        publisher = redis.createClient({
-                host: url, 
-                port: 6379,
-                legacyMode: true,
-        }); 
-
-        (async () => {
-            await publisher.connect();
-          })(); 
-
-        console.log("client created");
-
-        publisher.set("key", "value", redis.print);
-        publisher.get("key", redis.print);
-
-        publisher.on('error', (error) => {
-            console.log(error)
-        }); 
-
-        publisher.on('connect', () =>{
-            alert('Publisher is connected')
-        }); 
-    }
-
-  
-
-    // useEffect( () =>{
-    //     console.log("started")
-
-    //    try{
-
-    //     publisher = redis.createClient({
-    //         socket: {
-    //             host: url, 
-    //             port: 6379
-    //         }
-    //     });
-
-    //    }
-    //    catch(error){
-    //     console.log(error); 
-    //    }
-
-    //     publisher.on('connect', () =>{
-    //         console.log('publisher is connected')
-    //     } ) 
-
-    //     // subscriber.on('connect', () =>{
-    //     //     console.log('subscriber is connected')
-    //     // })
-
-    //     // subscriber.on('message', (channel, message) =>{
-    //     //     alert(`${message} FROM ${channel}`)
-    //     // })
-
-    //     console.log("ended")
-    // }, [refresh])
-
-    return(
-        <div>
-           
+   
+    return( 
+        <div  style = {{textAlign: "center"}}>
+            <br/> <br/> <br/>
+            <h3> PUBLISH MESSAGE</h3>
             <label> Channel Name: </label>
+            <input type = "text" onChange = {e => setChannelName(e.target.value)}/>
+            <br/><br/>
+            <label> Message: </label>
+            <input type = "text" onChange = {e => setMessage(e.target.value)}/>
+            <br/><br/>
+             <button onClick = {() => {publishMessage(channelName, message)} }> Send Message </button>
 
-            <button onClick = {createPublisher}> Create </button>
+            <h3> SUBSCRIBE </h3>
+            <label> Channel Name: </label> 
+            <input type = "text" onChange = { e => setSubChannelName(e.target.value)}/> 
+            <br/><br/>
+             <button onClick = {() =>{ subscribeChannel(subChannelName) }}> Subscribe </button>
+
+            <br/> <br/> 
+
+             <button onClick={sendMessage}> Send </button>
+
+           
         </div>
     ); 
 }
